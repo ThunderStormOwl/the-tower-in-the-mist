b2f72e23c9a1fd03218dd2a1c95ade662c298b6e
Phil <lausrock@gmail.com>
2021-03-07 23:43:11 -0300

    Reverse creation room order to make more sense in the hierarchy

--- CS Files/Scripts/TowerScript.cs
+++ CS Files/Scripts/TowerScript.cs
@@ -20,33 +20,44 @@ public class TowerScript : MonoBehaviour{
 
     void SetUpTower(){
         int counter = 0;
+        int floorCounter = 13;
 
-        double curX = 1.2;
-        double curY = 0.8;
+        double curX = 8.8;
+        double curY = 15.2;
 
         Vector3 pos = new Vector3((float)curX, (float)curY, 0);
         
         GameObject newRoom;
+        GameObject newFloor = new GameObject();
+        newFloor.name = "Floor " + floorCounter;
+        newFloor.transform.SetParent(gameObject.transform);
+        floorCounter--;
 
         for(int i = 0; i < maxRooms; i++){
             if(counter >= 4){
-                curY += 1.2;
-                curX = 1.2;
+                curY -= 1.2;
+                curX = 8.8;
                 pos.x = (float)curX;
                 pos.y = (float)curY;
                 counter = 0;
+                newFloor = new GameObject();
+                newFloor.name = "Floor " + floorCounter;
+                newFloor.transform.SetParent(gameObject.transform);
+                floorCounter--;
             }
             
             if(counter == 2)
-                curX += 3.4;
+                curX -= 3.4;
             else if(counter != 0)
-                curX += 2.1;
+                curX -= 2.1;
             pos.x = (float)curX;
 
             newRoom = Instantiate(blockedRoomPrefab, pos, Quaternion.identity) as GameObject;
+            newRoom.transform.SetParent(newFloor.transform);
             newRoom.GetComponent<BlockedRoomScript>().Initialize(2 + builtRooms.Count, this);
+            newRoom.name = "Room " + (maxRooms - blockedRooms.Count);
             blockedRooms.Add(newRoom);
-
+            
             counter += 1;
         }
     }
