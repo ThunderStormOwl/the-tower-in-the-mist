75bdd8bf38ccadf125414a018616de02d5eace10
ThunderStormOwl <lausrock@gmail.com>
2021-03-10 11:52:54 -0300

    test comment

--- CS Files/Scripts/TowerScript.cs
+++ CS Files/Scripts/TowerScript.cs
@@ -68,7 +68,7 @@ public class TowerScript : MonoBehaviour{
         
     }
 
-    public void BuildNewRoom(GameObject room){
+    public void BuildNewRoom(GameObject room){//change the name of this method
         GameObject newRoom = Instantiate(clearedRoomPrefab, room.transform.position, Quaternion.identity) as GameObject;
         
         if(room == roomWithUIOn)
