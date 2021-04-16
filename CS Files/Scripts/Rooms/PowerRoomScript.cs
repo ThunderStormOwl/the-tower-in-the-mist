dc60eb0bde6b8075645e558eb1fd8ac7c90081a2
ThunderStormOwl <lausrock@gmail.com>
2021-04-15 23:21:21 -0300

    created scripts for each major production room and did some organizing

---
+++ CS Files/Scripts/Rooms/PowerRoomScript.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerRoomScript : RoomClass{
+    void Start(){
+        
+    }
+    void Update(){
+        
+    }
+}
